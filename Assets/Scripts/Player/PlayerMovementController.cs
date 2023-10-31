d5cf3fb1c17505af61d406b802638cf809b626bb
gabrielucido <gabrielucido@gmail.com>
2023-10-31 12:11:27 -0400

    Player movement refactored

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerPhysicsController))]
@@ -5,23 +6,31 @@ public class PlayerMovementController : PlayerBase
 {
     private Rigidbody2D _rb;
     private PlayerPhysicsController _playerPhysicsController;
-
-    // private Vector2 _move = Vector2.zero;
     private Vector2 _frameVelocity;
     private bool _grounded;
+    private float _time;
 
-    // public event Action Jumped;
+    public event Action Jumped;
 
     protected override void Awake()
     {
         base.Awake();
         _rb = GetComponent<Rigidbody2D>();
         _playerPhysicsController = GetComponent<PlayerPhysicsController>();
+        _grounded = true;
+    }
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+        HandleJumpTimer();
     }
 
     void FixedUpdate()
     {
-        HandleMovement();
+        HandleJump();
+        HandleDirection();
+        HandleGravity();
         ApplyMovement();
     }
 
@@ -30,7 +39,7 @@ public class PlayerMovementController : PlayerBase
         _playerPhysicsController.GroundedChanged += OnGroundedChanged;
         _playerPhysicsController.HitCeiling += OnHitCeiling;
     }
-
+    
     private void OnDisable()
     {
         _playerPhysicsController.GroundedChanged -= OnGroundedChanged;
@@ -39,7 +48,7 @@ public class PlayerMovementController : PlayerBase
 
     #region Horizontal
 
-    private void HandleMovement()
+    private void HandleDirection()
     {
         if (Player.data.move.x == 0)
         {
@@ -58,54 +67,89 @@ public class PlayerMovementController : PlayerBase
 
     #endregion
 
-    // #region Jump
-    //
-    // public bool _jumpToConsume;
-    // public bool _bufferedJumpUsable;
-    //
-    // public bool _endedJumpEarly;
-    // private bool _coyoteUsable;
-    // private float _jumpPressedTime;
-    //
-    // private bool HasBufferedJump =>
-    //     _bufferedJumpUsable && Player.time < _jumpPressedTime + playerData.jumpBuffer;
-    //
-    // private bool CanUseCoyote =>
-    //     _coyoteUsable && !_grounded && _time < _frameLeftGrounded + playerData.coyoteTime;
-    //
-    // private void HandleJump()
-    // {
-    //     if (!_endedJumpEarly && !_grounded && !_frameInput.JumpHeld && _rb.velocity.y > 0) _endedJumpEarly = true;
-    //
-    //     if (!_jumpToConsume && !HasBufferedJump) return;
-    //
-    //     if (_grounded || CanUseCoyote) ExecuteJump();
-    //     _jumpToConsume = false;
-    // }
-    //
-    // private void ExecuteJump()
-    // {
-    //     _endedJumpEarly = false;
-    //     _jumpPressedTime = 0;
-    //     _bufferedJumpUsable = false;
-    //     _coyoteUsable = false;
-    //     _frameVelocity.y = Player.data.jumpPower;
-    //     Jumped?.Invoke();
-    // }
-    //
-    // #endregion
+    #region Jump
 
-    #region Event Handling
+    private bool _jumpToConsume;
+    private bool _bufferedJumpUsable;
+
+    private bool _endedJumpEarly;
+    private bool _coyoteUsable;
+    private float _jumpPressedTime;
+    private float _frameLeftGrounded;
+
+    private bool HasBufferedJump =>
+        _bufferedJumpUsable && _time < _jumpPressedTime + Player.data.jumpBuffer;
+
+    private bool CanUseCoyote =>
+        _coyoteUsable && !_grounded && _time < _frameLeftGrounded + Player.data.coyoteTime;
+
+    private void HandleJumpTimer()
+    {
+        if (!Player.data.jumpPressed) return;
+        _jumpPressedTime = _time;
+        _jumpToConsume = true;
+    }
+
+    private void HandleJump()
+    {
+        if (!_endedJumpEarly && !_grounded && !Player.data.jumpHeld && _rb.velocity.y > 0) _endedJumpEarly = true;
+
+        if (!_jumpToConsume && !HasBufferedJump) return;
+
+        if (_grounded || CanUseCoyote) ExecuteJump();
+        _jumpToConsume = false;
+    }
+
+    private void ExecuteJump()
+    {
+        _endedJumpEarly = false;
+        _jumpPressedTime = 0;
+        _bufferedJumpUsable = false;
+        _coyoteUsable = false;
+        _frameVelocity.y = Player.data.jumpPower;
+        Jumped?.Invoke();
+    }
+
+    #endregion
+
+    #region Gravity
+
+    private void HandleGravity()
+    {
+        if (_grounded && _frameVelocity.y <= 0f)
+        {
+            _frameVelocity.y = Player.data.groundingForce;
+        }
+        else
+        {
+            var inAirGravity = Player.data.fallAcceleration;
+            if (_endedJumpEarly && _frameVelocity.y > 0)
+                inAirGravity *= Player.data.jumpEndEarlyGravityModifier;
+            _frameVelocity.y =
+                Mathf.MoveTowards(_frameVelocity.y, -Player.data.maxFallSpeed,
+                    inAirGravity * Time.fixedDeltaTime);
+        }
+    }
+
+    #endregion
+
+    #region Events
 
     private void OnGroundedChanged(bool grounded)
     {
-        _grounded = grounded;
-        // if (grounded)
-        // {
-        //     _coyoteUsable = true;
-        //     _bufferedJumpUsable = true;
-        //     _endedJumpEarly = false;
-        // }
+        switch (_grounded)
+        {
+            case false when grounded:
+                _grounded = true;
+                _coyoteUsable = true;
+                _bufferedJumpUsable = true;
+                _endedJumpEarly = false;
+                break;
+            case true when !grounded:
+                _grounded = false;
+                _frameLeftGrounded = _time;
+                break;
+        }
     }
 
     private void OnHitCeiling()
